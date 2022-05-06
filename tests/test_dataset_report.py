e4b5f4fa39144827665e5fbbf6cdd5e79611ecf4
Iain Dillingham <iain@dillingham.me.uk>
2022-05-06 15:10:22 +0100

    Fix the `get_name` function
    Previously, the `get_name` function passed a file extension to the
    rstrip method. However, this method doesn't strip *all* characters
    passed to it; it strips *any* characters passed to it. For example:
    
    >>> name = "input.feather"
    >>> name.rstrip(".feather")
    'inpu'
    
    In this example, the file extension contains the letter "t", so rstrip
    strips the letter "t" from "input".
    
    Another example:
    
    >>> name = "input.csv"
    >>> name.rstrip(".csv")
    'input'
    
    In this example, the file extension has no letters in common with
    "input", so rstrip doesn't strip any letters from "input".

--- tests/test_dataset_report.py
+++ tests/test_dataset_report.py
@@ -1,2 +1,19 @@
-def test():
-    pass
+import pathlib
+
+import pytest
+
+from analysis import dataset_report
+
+
+@pytest.mark.parametrize(
+    "path,name",
+    [
+        (pathlib.Path("output/input.csv"), "input"),
+        (pathlib.Path("output/input.csv.gz"), "input"),
+        (pathlib.Path("output/input.feather"), "input"),
+        (pathlib.Path("output/input.dta"), "input"),
+        (pathlib.Path("output/input.dta.gz"), "input"),
+    ],
+)
+def test_get_name(path, name):
+    assert dataset_report.get_name(path) == name
