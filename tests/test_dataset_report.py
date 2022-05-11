f8dd0f81a64cc31cd905364b368a2b1db3cf9daf
Iain Dillingham <iain@dillingham.me.uk>
2022-05-11 13:04:55 +0100

    Recreate the bug with a test

--- tests/test_dataset_report.py
+++ tests/test_dataset_report.py
@@ -1,8 +1,22 @@
 import pathlib
 
+import pandas
 import pytest
 
 from analysis import dataset_report
+
+
+@pytest.fixture
+def dataframe():
+    return pandas.DataFrame(
+        {
+            "patient_id": [1],
+            "is_registered": [True],
+            "is_dead": [False],
+            "stp_code": ["STP0"],
+            "has_sbp_event": [True],
+        }
+    )
 
 
 @pytest.mark.parametrize(
@@ -17,3 +31,12 @@ from analysis import dataset_report
 )
 def test_get_name(path, name):
     assert dataset_report.get_name(path) == name
+
+
+@pytest.mark.xfail(
+    reason="The argument and the return value share an index instance",
+    strict=True,
+)
+def test_get_memory_usage(dataframe):
+    memory_usage = dataset_report.get_memory_usage(dataframe)
+    assert dataframe.columns is not memory_usage.index
